16f3b3858c4207d6cc6c2a79eb0a8eabeee9cfd4
Craniumski <tycho@compuguru.com>
2016-10-22 14:46:33 -0500

    Mix inputs to add turning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_6832.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_6832.java
@@ -120,16 +120,20 @@ public class TeleOp_6832 extends LinearOpMode {
             powerBackRight = gamepad1.left_stick_y;
         }
         if(Math.abs(gamepad1.left_stick_x) >= .10){
-            powerFrontLeft += gamepad1.left_stick_x;
-            powerFrontRight += -gamepad1.left_stick_x;
-            powerBackLeft += -gamepad1.left_stick_x;
-            powerBackRight += gamepad1.left_stick_x;
+            powerFrontLeft += -gamepad1.left_stick_x;
+            powerFrontRight += gamepad1.left_stick_x;
+            powerBackLeft += gamepad1.left_stick_x;
+            powerBackRight += -gamepad1.left_stick_x;
         }
 
 
         if(Math.abs(gamepad1.right_stick_x) >= .10){
-
+            powerFrontLeft += gamepad1.right_stick_x;
+            powerBackLeft += gamepad1.right_stick_x;
+            powerFrontRight -= gamepad1.right_stick_x;
+            powerBackRight -= gamepad1.right_stick_x;
         }
+
 
         motorFrontLeft.setPower(clampMotor(powerFrontLeft));
         motorBackLeft.setPower(clampMotor(powerBackLeft));
