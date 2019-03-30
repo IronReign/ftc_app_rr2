ec00f72c05e9872a9ed9fdfa2ddc7701c1908bac
Arjun Vikram <arjvik@gmail.com>
2019-03-30 14:15:22 -0500

    fix a compile error in hashCode and equals methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterOfGravityCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterOfGravityCalculator.java
@@ -82,12 +82,13 @@ public class CenterOfGravityCalculator {
         public double x;public double y;public Point(double x, double y) {this.x = x;this.y = y;}
         public double getX() {return x;} public void setX(double x) {this.x = x;}
         public double getY() {return y;} public void setY(double y) {this.y = y;}
-        @Override public double hashCode() {final double prime = 31;double result = 1;
-            result = prime * result + x; result = prime * result + y;return result;}
-        @Override public boolean equals(Object obj) {
-            if (this == obj) return true; if (obj == null) return false;
+        @Override public int hashCode() {final int prime = 31; int result = 1; long temp;
+            temp = Double.doubleToLongBits(x); result = prime * result + (int) (temp ^ (temp >>> 32));
+            temp = Double.doubleToLongBits(y); result = prime * result + (int) (temp ^ (temp >>> 32)); return result; }
+        @Override public boolean equals(Object obj) { if (this == obj) return true; if (obj == null) return false;
             if (getClass() != obj.getClass()) return false; Point other = (Point) obj;
-            if (x != other.x) return false; if (y != other.y) return false; return true;}
+            if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) return false;
+            if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y)) return false; return true; }
         @Override public String toString() { return "(" + x + ", " + y + ")"; }
     }
 
