c1b4d4015afe42490242563aa43f00102bc49976
Arjun Vikram <arjvik@users.noreply.github.com>
2019-03-31 09:58:10 -0500

    Make COG code use the correct angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game_6832.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game_6832.java
@@ -1143,7 +1143,7 @@ public class Game_6832 extends LinearOpMode {
                 .addData("supermanPos", () -> robot.superman.getCurrentPosition())
                 .addData("liftPos", () -> robot.collector.getExtendABobCurrentPos());
         telemetry.addLine()
-                .addData("COG: ", () -> robot.cog.getCenterOfGravity(robot.getRoll() +robot.cog.pitchOffset,robot.superman.getCurrentAngle() + robot.cog.supermanoffset, robot.collector.getCurrentAngle()+robot.cog.elbowoffset, .382).toString());
+                .addData("COG: ", () -> robot.cog.getCenterOfGravity(robot.getRoll() +robot.cog.pitchOffset,180-(robot.superman.getCurrentAngle() + robot.cog.supermanoffset), robot.collector.getCurrentAngle()+robot.cog.elbowoffset, .382).toString());
         telemetry.addLine()
                 .addData("roll", () -> robot.getRoll())
                 .addData("pitch", () -> robot.getPitch())
