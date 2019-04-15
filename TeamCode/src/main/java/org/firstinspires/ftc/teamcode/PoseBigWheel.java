4852dd73f92a6a75bc4d241966ec0bb9115c3059
Arjun Vikram <arjvik@gmail.com>
2019-04-15 17:36:33 -0500

    fix compile error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseBigWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseBigWheel.java
@@ -901,7 +901,6 @@ public class PoseBigWheel
                            miniState = 0; //just being a good citizen for next user of miniState
                            articulation = Articulation.manual; //force end of articulation by switching to manual
                            return Articulation.manual;
-                           break;
                    }
                }else{
                    collector.extendToMin(1,10);
